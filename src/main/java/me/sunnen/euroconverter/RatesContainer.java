780c2213a218dc8cbb9924d120fecf8655f191b8
Dany <netfox2006@hotmail.com>
2015-12-13 20:49:07 +0100

    Optimised rates container to synchronized only when needed

--- src/main/java/me/sunnen/euroconverter/RatesContainer.java
+++ src/main/java/me/sunnen/euroconverter/RatesContainer.java
@@ -3,6 +3,7 @@ package me.sunnen.euroconverter;
 import java.io.File;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -40,12 +41,53 @@ public class RatesContainer
 		latestDate = LocalDate.of(2000, 1, 1);
 	}
 	
+	private boolean needsSynchronized()
+	{
+		LocalDateTime ldt = LocalDateTime.now();
+		int minute = ldt.getMinute();
+		int hour = ldt.getHour();
+		return ((hour==15) && (minute<=20)) ? true : false;
+	}
+	
 	public void addEntriesFromFile(File file, boolean removeEarliestKey)
 	{
+		boolean sync = needsSynchronized();
 		if (removeEarliestKey)
 		{
-			synchronized(this)
-     		{
+			if(sync)
+			{
+				synchronized(this)
+				{
+					{
+						if (rateMap!=null)
+						{
+							//Remove the given key
+							rateMap.remove(earliestDate.toString());
+							LocalDate nextDate = earliestDate.plusDays(1);
+							int nTries=0;
+							//Attempt to find the earliest key present
+							while(!rateMap.containsKey(nextDate.toString()) && nTries<maxTries)
+							{
+								nTries++;
+								nextDate = nextDate.plusDays(1);
+							}
+							if (nTries!=maxTries)
+							{
+								//OK found the earliest date
+								earliestDate = nextDate;
+							}
+							else
+							{
+								//Earliest date was not found
+								System.out.println("Could not find new earliest date!");
+								return;
+							}
+						}
+		     		}
+				}
+			}
+			else
+			{
 				if (rateMap!=null)
 				{
 					//Remove the given key
@@ -70,7 +112,8 @@ public class RatesContainer
 						return;
 					}
 				}
-     		}
+			}
+     		
 		}
 		try
 		{
@@ -104,27 +147,51 @@ public class RatesContainer
 	             		//Check if date is already in RatesContainer: if yes skip
              			if (rateMap.containsKey(dateAttribute.getNodeValue())) continue;
 	             		currencyNodes = dateNode.getChildNodes();
-	             		synchronized(this)
+	             		if (sync)
 	             		{
-	             			currencyMap = new HashMap<String,String>();
-		             		for(int j=0;j<currencyNodes.getLength();j++)
+	             			synchronized(this)
 		             		{
-		             			//Loop through the currencies
-		             			currencyNode = currencyNodes.item(j);
-		             			if (currencyNode.getNodeType()!=Node.ELEMENT_NODE) continue;
-		             			attributes = currencyNode.getAttributes();
-		             			currencyAttribute = attributes.getNamedItem("currency");
-		             			rateAttribute = attributes.getNamedItem("rate");
-		             			//Add a currency, rate key-value pair to the HashMap
-		             			currencyMap.put(currencyAttribute.getNodeValue(), rateAttribute.getNodeValue());
+		             			currencyMap = new HashMap<String,String>();
+			             		for(int j=0;j<currencyNodes.getLength();j++)
+			             		{
+			             			//Loop through the currencies
+			             			currencyNode = currencyNodes.item(j);
+			             			if (currencyNode.getNodeType()!=Node.ELEMENT_NODE) continue;
+			             			attributes = currencyNode.getAttributes();
+			             			currencyAttribute = attributes.getNamedItem("currency");
+			             			rateAttribute = attributes.getNamedItem("rate");
+			             			//Add a currency, rate key-value pair to the HashMap
+			             			currencyMap.put(currencyAttribute.getNodeValue(), rateAttribute.getNodeValue());
+			             		}
+			             		currentDate=LocalDate.parse(dateAttribute.getNodeValue());
+			             		//Add the currency map to the entire hash map
+			             		rateMap.put(dateAttribute.getNodeValue(), currencyMap);
+			             		//Adjust earliest and latest dates
+			             		if (currentDate.compareTo(earliestDate)<0) earliestDate = currentDate;
+				             	if (currentDate.compareTo(latestDate)>0) latestDate = currentDate;
 		             		}
-		             		currentDate=LocalDate.parse(dateAttribute.getNodeValue());
-		             		//Add the currency map to the entire hash map
-		             		rateMap.put(dateAttribute.getNodeValue(), currencyMap);
-		             		//Adjust earliest and latest dates
-		             		if (currentDate.compareTo(earliestDate)<0) earliestDate = currentDate;
-			             	if (currentDate.compareTo(latestDate)>0) latestDate = currentDate;
 	             		}
+	             		else
+	             		{
+		             		currencyMap = new HashMap<String,String>();
+			             	for(int j=0;j<currencyNodes.getLength();j++)
+			             	{
+			             		//Loop through the currencies
+			             			currencyNode = currencyNodes.item(j);
+			             			if (currencyNode.getNodeType()!=Node.ELEMENT_NODE) continue;
+			             			attributes = currencyNode.getAttributes();
+			             			currencyAttribute = attributes.getNamedItem("currency");
+			             			rateAttribute = attributes.getNamedItem("rate");
+			             			//Add a currency, rate key-value pair to the HashMap
+			             			currencyMap.put(currencyAttribute.getNodeValue(), rateAttribute.getNodeValue());
+			             	}
+			             	currentDate=LocalDate.parse(dateAttribute.getNodeValue());
+			             	//Add the currency map to the entire hash map
+			             	rateMap.put(dateAttribute.getNodeValue(), currencyMap);
+			             	//Adjust earliest and latest dates
+			             	if (currentDate.compareTo(earliestDate)<0) earliestDate = currentDate;
+				            if (currentDate.compareTo(latestDate)>0) latestDate = currentDate;
+		             	}
 	             	}
 	     		}
 	     	}
@@ -148,7 +215,49 @@ public class RatesContainer
 	
 	public Rate getRate(String currency, String date)
 	{
-		synchronized(this)
+		boolean sync = needsSynchronized();
+		if (sync)
+		{
+			synchronized(this)
+			{
+				//Check if the given date is in the RatesContainer
+	 			if (!rateMap.containsKey(date))
+	 			{
+	 				//Default to latest date
+	 				Map<String,String> m = rateMap.get(latestDate.toString());
+	 				if (m==null) return new Rate(latestDate.toString(),defaultCurrency,"error"); //error latest date not found
+	 				if (m.containsKey(currency))
+	 				{
+	 					String rate = m.get(currency);
+	 					return new Rate(latestDate.toString(),currency,rate);
+	 				}
+	 				else
+	 				{
+	 					//Default to default currency
+	 					String rate = m.get(defaultCurrency);
+	 					if (rate==null) return new Rate(latestDate.toString(),defaultCurrency,"error"); //error getting value for default currency
+	 					else return new Rate(latestDate.toString(),currency,rate);
+	 				}
+	 			}
+	 			else //OK given date is in RateContainer
+	 			{
+	 				Map<String,String> m = rateMap.get(date);
+	 				if (m.containsKey(currency))
+	 				{
+	 					String rate = m.get(currency);
+	 					return new Rate(date,currency,rate);
+	 				}
+	 				else
+	 				{
+	 					//Default to default currency
+	 					String rate = m.get(defaultCurrency);
+	 					if (rate==null) return new Rate(date,defaultCurrency,"error"); //error getting value for default currency
+	 					else return new Rate(date,currency,rate);
+	 				}
+	 			}
+			}
+		}
+		else
 		{
 			//Check if the given date is in the RatesContainer
  			if (!rateMap.containsKey(date))
@@ -190,9 +299,17 @@ public class RatesContainer
 	
 	public String[] getCurrencies()
 	{
+		boolean sync = needsSynchronized();
 		String result[];
 		Map<String,String> map;
-		synchronized(this)
+		if (sync)
+		{
+			synchronized(this)
+			{
+				map = rateMap.get(latestDate.toString());
+			}
+		}
+		else
 		{
 			map = rateMap.get(latestDate.toString());
 		}
@@ -211,8 +328,25 @@ public class RatesContainer
 	
 	public String[] getDates()
 	{
+		boolean sync = needsSynchronized();
 		String result[];
-		synchronized(this)
+		if (sync)
+		{
+			synchronized(this)
+			{
+				Set<String> set = rateMap.keySet();
+				int size = set.size();
+				result = new String[size];
+				Iterator<String> it = set.iterator();
+				int i=0;
+				while(it.hasNext())
+				{
+					result[i] = it.next();
+					i++;
+				}
+			}
+		}
+		else
 		{
 			Set<String> set = rateMap.keySet();
 			int size = set.size();
